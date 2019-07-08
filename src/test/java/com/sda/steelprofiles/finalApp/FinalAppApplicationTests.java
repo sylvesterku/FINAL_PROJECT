57d6820c40ec7a0568df56da2e73bfa6dfd541f9
SylwesterKu <skukla89@gmail.com>
2019-07-08 20:45:59 +0200

    Create project2

---
+++ src/test/java/com/sda/steelprofiles/finalApp/FinalAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sda.steelprofiles.finalApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinalAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
